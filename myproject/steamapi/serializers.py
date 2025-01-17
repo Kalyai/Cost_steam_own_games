92885d711c512ce3a8bd621d8ad870627ec676af
Sergei Kalyaev <sergei.kalyaev0007@gmail.com>
2025-01-17 17:06:42 +0300

    Delete files wich write in .gitignore

--- myproject/steamapi/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import SteamUser
-
-class SteamUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SteamUser
-        fields = '__all__'
